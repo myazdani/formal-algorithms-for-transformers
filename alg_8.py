cc663fe7b33594cb0c6dcd4195a6129e5dc76f9f
Mehrdad Yazdani <myazdani@gmail.com>
2023-03-03 23:49:47 -0800

    added decoder layers and unembedding layer

--- alg_8.py
+++ alg_8.py
@@ -6,6 +6,7 @@ from alg_2 import PositionEmbedding
 from alg_1 import TokenEmbedding
 from alg_5 import MHAttentionInefficient
 from alg_6 import LayerNorm
+from alg_7 import TokenUnembedding
 
 
 class EDTransformer(nn.Module):
@@ -15,6 +16,7 @@ class EDTransformer(nn.Module):
         self.max_seq_len = max_seq_len
         self.token_emb = TokenEmbedding(vocab_size, embed_dim)
         self.pos_emb = PositionEmbedding(max_seq_len, embed_dim)
+        # setup encoder
         encoder_layers = []
         for i in range(L_enc):
             multi_head_attention = MHAttentionInefficient(num_heads=num_heads,
@@ -31,7 +33,31 @@ class EDTransformer(nn.Module):
             encoder_layers.append(fc2)
             layer_norm_2 = LayerNorm(embed_dim)
             encoder_layers.append(layer_norm_2)
-    
         self.encoder = nn.Sequential(*encoder_layers)
-        
+        # setup decoder
+        decoder_layers = []
+        for i in range(L_dec):
+            multi_head_attention = MHAttentionInefficient(num_heads=num_heads,
+                                                          input_dim=embed_dim,
+                                                          attention_dim=embed_dim,
+                                                          output_dim=embed_dim)   
+            decoder_layers.append(multi_head_attention)
+            layer_norm_3 = LayerNorm(embed_dim)
+            decoder_layers.append(layer_norm_3)
+            multi_head_attention = MHAttentionInefficient(num_heads=num_heads,
+                                                          input_dim=embed_dim,
+                                                          attention_dim=embed_dim,
+                                                          output_dim=embed_dim)  
+            decoder_layers.append(multi_head_attention)
+            layer_norm_4 = LayerNorm(embed_dim)
+            decoder_layers.append(layer_norm_4)
+            fc3 = nn.Linear(embed_dim, mlp_dim)
+            decoder_layers.append(fc3)
+            decoder_layers.append(nn.ReLU())
+            fc4 = nn.Linear(mlp_dim, embed_dim)
+            decoder_layers.append(fc4)
+            layer_norm_5 = LayerNorm(embed_dim)
+            decoder_layers.append(layer_norm_5)
+        self.decoder = nn.Sequential(*decoder_layers)
 
+        self.unembed = TokenUnembedding(vocab_size, embed_dim)
