ea805d15adceaec721d8ce5449ed279b5e4246fe
Mehrdad Yazdani <myazdani@gmail.com>
2023-03-14 09:50:29 -0700

    corrected shapes

--- src/alg_4.py
+++ src/alg_4.py
@@ -15,19 +15,19 @@ class Attention(SingleQueryAttention):
         # self.register_buffer("masked_bias", torch.tensor(-1e4))
 
     def forward(self, current_tokens, context_tokens, attention_mask=None):
-        q = self.query(current_tokens).T
-        k = self.key(context_tokens).T
-        v = self.value(context_tokens).T
-        att = torch.einsum('ijk,ijk->ik', [q,k]) / math.sqrt(self.atten_dim)
+        q = self.query(current_tokens)
+        k = self.key(context_tokens)
+        v = self.value(context_tokens)
+        att = torch.einsum('ijk,ilk->ijl', [q,k]) / math.sqrt(self.atten_dim)
         # hugging face implementation: https://tinyurl.com/22f9b6y
         if attention_mask is not None:
             # Apply the attention mask
             # att = torch.where(mask, att, self.masked_bias)
-            att = att + attention_mask
+            att = att + attention_mask[None,:,:]
         
         att = F.softmax(att, dim=-1)
-        v = torch.einsum('ijk,ik->jk',[v, att])
-        return v.T
+        v = torch.einsum('ijk,ijl->ijk',[v, att])
+        return v
 
 def main():
     max_seq_len = 512
@@ -35,17 +35,16 @@ def main():
     vocab_size = 10000
     token_emb = TokenEmbedding(vocab_size, embed_dim)
     pos_emb = PositionEmbedding(max_seq_len, embed_dim)
-    attention = Attention(50, 50, 50)
-
-    idx = torch.tensor([43, 32])  
+    attention = Attention(embed_dim, embed_dim, embed_dim)
+    batch_size=32
+    idx = torch.randint(0,vocab_size, size = (batch_size, max_seq_len)) 
     token_embeddings = token_emb(idx)
-    position_embeddings = pos_emb(2)
-    x = position_embeddings + token_embeddings
+    position_embeddings = pos_emb(max_seq_len)
+    x = position_embeddings[None,:,:] + token_embeddings
     print(x.shape)
-    mask = torch.tril(torch.ones(len(idx), len(idx)))
+    mask = torch.tril(torch.ones(max_seq_len, max_seq_len))
     x_emb = attention(x,x, mask.masked_fill(mask==0, float('-inf')))
     print(x_emb.shape)
-    print(x_emb)
 
 
 if __name__ == "__main__":
